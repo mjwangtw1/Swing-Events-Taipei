2d7f1ccbb34f61fe04736b1de3d2026c6bedffe3
Mou.wang <mou.wang@uitox.com>
2016-05-13 15:56:57 +0800

    testing to work with ustream

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -1,5 +1,3 @@
-Live Testing Page.
+Live Page (Test)
 
-
-
-<iframe width="480" height="270" src="http://www.ustream.tv/channel/13628076?html5ui" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>
+<iframe src="http://www.ustream.tv/embed/15479191?html5ui&volume=0&showtitle=false" style="border: 0 none transparent;"  webkitallowfullscreen allowfullscreen frameborder="no" width="720" height="405"></iframe><br /><a href="http://www.ustream.tv/" style="padding: 2px 0px 4px; width: 400px; background: #ffffff; display: block; color: #000000; font-weight: normal; font-size: 10px; text-decoration: underline; text-align: center;" target="_blank">Live streaming video by Ustream</a>
