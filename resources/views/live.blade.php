b22287709fb8706a067b1a832852311209e9fadd
Mou.wang <mou.wang@uitox.com>
2016-05-13 16:48:24 +0800

    add some wording

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -1,3 +1,5 @@
 Live Page (Test)
 
+<h3> Today's Events: (Swing) Big Apple Indoor Social </h3>
+
 <iframe src="http://www.ustream.tv/embed/15479191?html5ui&volume=0&showtitle=false" style="border: 0 none transparent;"  webkitallowfullscreen allowfullscreen frameborder="no" width="720" height="405"></iframe><br /><a href="http://www.ustream.tv/" style="padding: 2px 0px 4px; width: 400px; background: #ffffff; display: block; color: #000000; font-weight: normal; font-size: 10px; text-decoration: underline; text-align: center;" target="_blank">Live streaming video by Ustream</a>
