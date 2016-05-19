cfe3fc83291b11e5a023eeb7ddda1c952941ce13
Yuming Cheung <ymcheung.tw@gmail.com>
2016-05-19 17:55:47 +0800

    Home layout redesign

--- resources/views/event_display/home.blade.php
+++ resources/views/event_display/home.blade.php
@@ -12,23 +12,26 @@ $weekday['tw'] = ['周日', '周一','周二','周三','周四','周五','周六
 @extends('layouts.swing')
 
 @section('content')
-    <header class="row">
+    <header class="page__home">
 
-        <div class="the_logo small-12 columns">
-            <h1 class="logo_text">
-                Swing Events
-            </h1>
-            <div class="logo_taipei">
-                &nbsp;
+        <div class="row">
+
+            <div class="the_logo small-12 columns">
+                <h1 class="logo_text">
+                    Swing Events
+                </h1>
+                <div class="logo_taipei">
+                    &nbsp;
+                </div>
             </div>
-        </div>
-
-        <div class="the_menu small-12 columns hide-for-medium">
-            <div class="row align-center">
-                
-                <!-- Cluster for Promo Events -->
-                <div class="small-4 columns">
-                    <div class="special_promo">
+            
+            
+            <div class="the_menu small-12 columns hide-for-medium">
+                <div class="row align-center">
+            
+                    <!-- Cluster for Promo Events -->
+                    <div class="small-4 columns">
+                        <div class="special_promo">
                         <!-- <a href="javascript:void(0)">
                             <span>
                                 Taipei Lindy Festival 2016
@@ -56,11 +59,17 @@ $weekday['tw'] = ['周日', '周一','周二','周三','周四','周五','周六
                         </h6>
                     </a> -->
                 </div>
-
             </div>
+
         </div>
 
     </header>
+
+
+    <div class="featured_cover">
+        &nbsp;
+    </div>
+
 
     <div class="content row">
         
@@ -87,30 +96,24 @@ $weekday['tw'] = ['周日', '周一','周二','周三','周四','周五','周六
 
         ?>
 
-        <div class="the_event is__featured small-12 medium-6 columns">
+        <div class="the_event is__featured small-12 medium-5 medium-offset-1 columns">
             
-            <a href="{{ $special_link }}" class="featured_cover row align-right" >
-                <!-- 活動詳情按鈕(指示用，連結在上層) -->
-                <!-- <div class="small-6 columns"> -->
-                <div class="feature_tags">
- <!--                    <span class="label is__special">Special</span> -->
-                    <span class="label {{ $event_type_dom[$special_type] }}">{{ $event_type[$special_type] }}</span>
+            <a href="{{ $special_link }}" class="description_box row small-collapse align-center">
+                <!-- 活動標題 -->
+                <h3 class="event_header for__feature_title small-12 columns">
+
+                    {{ $special_event['summary'] }}
+
+                    <span class="label {{ $event_type_dom[$special_type] }} for__home">{{ $event_type[$special_type] }}</span>
     
-                    @if( $featured_flag)
+                    <!-- @if( $featured_flag)
 
                         <span class="label to__detail">
                            {{ trans('default.featured') }}
                             <i class="icon o_vt s_small is__chevy_right"></i>
                         </span>
                     
-                    @endif
-
-                </div>
-            </a>
-            <div class="description_box row small-collapse align-center">
-                <!-- 活動標題 -->
-                <h3 class="event_header for__feature_title small-12 columns">
-                     {{ $special_event['summary'] }}
+                    @endif -->
                 </h3>
                 <div class="small-3 medium-2 columns">
                     <div class="date_calendar">
@@ -168,139 +171,139 @@ $weekday['tw'] = ['周日', '周一','周二','周三','周四','周五','周六
                         </span>
                     </div>
                 </div>
-            </div>
+            </a>
 
-            <div class="swing_intro row hide-for-small show-for-medium">
-                <div class="small-12 columns">
-                    <h4>
-                        哈囉！台北
-                    </h4>
-                    <hr>
-                    <p>
-                        Swing dance (搖擺舞)是一種復古舞蹈，源於 1930 年代的美國。它充滿了樂趣，而且保證跳完後你一定開心得闔不攏嘴！
-                    </p>
-                    <p>
-                        如果你喜歡爵士音樂或復古文化，更或者只是想度過一段美好時光，都可以﻿免費參加各個團體的練習時間﻿。不用擔心你之前是否跳過，我相信一定會有舞者帶你入門的。
-                    </p>
-                    <p>
-                        我們喜歡這個舞蹈，相信你們也會喜歡。來跟我們一起跳舞吧！
-                    </p>
-                </div>
-            </div>
         </div>
 
-        <!-- Thisweek -->
-        <div class="small-12 medium-6 columns">
-
-            @if ( ! empty($data))
-                
-            <div class="the_event is__thisweek">
-                <h6>
-                    {{ trans('default.event_this_week') }}
-                </h6>
-                
-                  @foreach($data as $type => $event)
-
-                    @foreach($event['events'] as $single_event)
-
-                <div class="event_box row small-collapse align-center">
-                    <!-- 信封封面 -->
-                    <div class="envelop_body small-10 medium-8 columns">
-                        <div class="row small-collapse align-right">
-                            <a href="/event/{{ $event['calendarId'] }}/{{ $single_event['id']}}/{{$type}}" class="cover_box small-10 columns">
-                                <h5>
-                                    {{ $single_event['summary'] }}
-                                </h5>
-                                <div class="description_box row small-collapse align-center">
-                                    <div class="small-3 columns">
-                                        <div class="date_calendar">
-                                            <!-- 月份 -->
-                                            <div class="for__month">
-                                                   {{ isset($single_event['start']['dateTime']) ? $dt->parse($single_event['start']['dateTime'])->format('M') : '' }}  
-                                            </div>
-                                            <!-- 日期 -->
-                                            <div class="for__day">
-                                                {{ isset($single_event['start']['dateTime']) ? $dt->parse($single_event['start']['dateTime'])->format('d') : '' }} 
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="small-7 columns">
-                                        <div class="date_clock">
-                                            <?php
-
-                                                if (isset($single_event['start']['dateTime']))
-                                                {
-                                                    $weekday_counter = $dt->parse($single_event["start"]["dateTime"])->format("w");
-                                                
-                                                    $lang_type = (Session::get('locale') === "tw") ? 'tw' : 'en';
-                                                    
-                                                    echo $weekday[$lang_type][$weekday_counter];
-                                                }
-                                            ?>
-                                            <!-- 視覺分隔點(全型的點)-->
-                                            ．
-                                            <!-- 相對日期 -->
-                                            <span class="for__relateday">
-
-                                                 <?php
-
-                                                if (isset($single_event['start']['dateTime']))
-                                                {
-                                                    if($dt->parse($single_event['start']['dateTime'])->isToday())
-                                                    {
-                                                        echo trans('default.today'); 
-                                                    }
-                                                    else
-                                                    {
-                                                        //Calculate the Difference.
-                                                        $count = $dt->diffInDays($dt->parse($single_event['start']['dateTime'])) + 1; //Start from 0 so add 1
-                                                        echo trans('default.days_till', ['count' => $count]);
-                                                    }
-                                                 }
-                                                ?>
-
-                                            </span>
-                                            <!-- 視覺分隔點 -->
-                                            ．
-                                            <!-- 當天時間 -->
-                                            <span class="for__oclock">
-                                                {{ isset($single_event['start']['dateTime']) ? $dt->parse($single_event['start']['dateTime'])->format('H:i') : '' }} 
-                                            </span>
-                                        </div>
-                                        <!-- 地點 -->
-                                        <div class="for__placename">
-                                            <i class="icon is__pin_b o_vt s_small">&nbsp;</i>
-                                            <span>{{ isset($single_event['location']) ? $single_event['location'] : '' }}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="the_category small-12 columns">
-                                        <span class="label {{ $event_type_dom[$type] }}">{{ $event_type[$type] }}</span>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-
-                    <!-- 信封開口 -->
-                    <!-- 21:25 在這裡加上 is__special 之類的標籤 -->
-                    <div class="envelop_open {{ $event_type_dom[$type] }} small-2 columns">
-                        &nbsp;
-                    </div>
-                </div>
-                
-                    @endforeach
-
-                @endforeach
-
-            </div>
-            
-            @else
-                <!-- NO class TEXT -->
-            @endif
+        <div class="swing_intro small-6 columns hide-for-small show-for-medium">
+            <h4>
+                哈囉！台北
+            </h4>
+            <hr>
+            <p>
+                Swing dance (搖擺舞)是一種復古舞蹈，源於 1930 年代的美國。它充滿了樂趣，而且保證跳完後你一定開心得闔不攏嘴！
+            </p>
+            <p>
+                如果你喜歡爵士音樂或復古文化，更或者只是想度過一段美好時光，都可以﻿免費參加各個團體的練習時間﻿。不用擔心你之前是否跳過，我相信一定會有舞者帶你入門的。
+            </p>
+            <p>
+                我們喜歡這個舞蹈，相信你們也會喜歡。來跟我們一起跳舞吧！
+            </p>
         
         </div>
+    
+    </div>
+
+    <!-- Thisweek -->
+    <div class="row columns small-collapse">
+
+        @if ( ! empty($data))
+            
+        <div class="the_event is__thisweek small-12 medium-5 medium-offset-1 columns">
+            <h6>
+                {{ trans('default.event_this_week') }}
+            </h6>
+            
+              @foreach($data as $type => $event)
+
+                @foreach($event['events'] as $single_event)
+
+            <div class="event_box row small-collapse align-center">
+                <!-- 信封封面 -->
+                <div class="envelop_body small-10 medium-8 columns">
+                    <div class="row small-collapse align-right">
+                        <a href="/event/{{ $event['calendarId'] }}/{{ $single_event['id']}}/{{$type}}" class="cover_box small-10 columns">
+                            <h5>
+                                {{ $single_event['summary'] }}
+                            </h5>
+                            <div class="description_box row small-collapse align-center">
+                                <div class="small-3 columns">
+                                    <div class="date_calendar">
+                                        <!-- 月份 -->
+                                        <div class="for__month">
+                                               {{ isset($single_event['start']['dateTime']) ? $dt->parse($single_event['start']['dateTime'])->format('M') : '' }}  
+                                        </div>
+                                        <!-- 日期 -->
+                                        <div class="for__day">
+                                            {{ isset($single_event['start']['dateTime']) ? $dt->parse($single_event['start']['dateTime'])->format('d') : '' }} 
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="small-7 columns">
+                                    <div class="date_clock">
+                                        <?php
+
+                                            if (isset($single_event['start']['dateTime']))
+                                            {
+                                                $weekday_counter = $dt->parse($single_event["start"]["dateTime"])->format("w");
+                                            
+                                                $lang_type = (Session::get('locale') === "tw") ? 'tw' : 'en';
+                                                
+                                                echo $weekday[$lang_type][$weekday_counter];
+                                            }
+                                        ?>
+                                        <!-- 視覺分隔點(全型的點)-->
+                                        ．
+                                        <!-- 相對日期 -->
+                                        <span class="for__relateday">
+
+                                             <?php
+
+                                            if (isset($single_event['start']['dateTime']))
+                                            {
+                                                if($dt->parse($single_event['start']['dateTime'])->isToday())
+                                                {
+                                                    echo trans('default.today'); 
+                                                }
+                                                else
+                                                {
+                                                    //Calculate the Difference.
+                                                    $count = $dt->diffInDays($dt->parse($single_event['start']['dateTime'])) + 1; //Start from 0 so add 1
+                                                    echo trans('default.days_till', ['count' => $count]);
+                                                }
+                                             }
+                                            ?>
+
+                                        </span>
+                                        <!-- 視覺分隔點 -->
+                                        ．
+                                        <!-- 當天時間 -->
+                                        <span class="for__oclock">
+                                            {{ isset($single_event['start']['dateTime']) ? $dt->parse($single_event['start']['dateTime'])->format('H:i') : '' }} 
+                                        </span>
+                                    </div>
+                                    <!-- 地點 -->
+                                    <div class="for__placename">
+                                        <i class="icon is__pin_b o_vt s_small">&nbsp;</i>
+                                        <span>{{ isset($single_event['location']) ? $single_event['location'] : '' }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="the_category small-12 columns">
+                                    <span class="label {{ $event_type_dom[$type] }}">{{ $event_type[$type] }}</span>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <!-- 信封開口 -->
+                <!-- 21:25 在這裡加上 is__special 之類的標籤 -->
+                <div class="envelop_open {{ $event_type_dom[$type] }} small-2 columns">
+                    &nbsp;
+                </div>
+            </div>
+            
+                @endforeach
+
+            @endforeach
+
+        </div>
+        
+        @else
+            <!-- NO class TEXT -->
+        @endif
     
     </div>
 
