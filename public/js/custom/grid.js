bfdded672a4d324b107b2f65b73c4afe9413ea3b
Mou.wang <mou.wang@uitox.com>
2016-06-01 23:46:52 +0800

    add a sample

--- public/js/custom/grid.js
+++ public/js/custom/grid.js
@@ -1,6 +1,9 @@
 //Doc Ready;
 $(function() 
 {
+    var load_image = 'http://www.jqueryscript.net/images/jQuery-Ajax-Loading-Overlay-with-Loading-Text-Spinner-Plugin.jpg';
+    var fail_image = 'http://acornishgirl.com/wp-content/uploads/2014/02/Failed-580x390.jpg';
+
     //Sample Data
     var items = [
           { title: 'Cat 1', image: 'http://placekitten.com/311/313' },
@@ -11,22 +14,31 @@ $(function()
           { title: 'Cat 6', image: 'http://placekitten.com/306/306' },
           { title: 'Cat 7', image: 'http://placekitten.com/307/307' },
           { title: 'Cat 8', image: 'http://placekitten.com/308/308' },
-          { title: 'Cat 9', image: 'http://placekitten.com/310/310' }
+          { title: 'Cat 9', image: 'http://placekitten.com/310/310' },
+          { title: 'Cat 9', image: 'http://placekitte.com/310/310' }
     ];
 
     console.log('===This is grid demo=== 2244');
 
-    //jQuery Foreach to loop through
-    $.each( items, function( key, value ) 
+
+    //Pre-load //Fill with Load image.
+    $('.img').attr("src", load_image);
+
+    $('.load').on('click',function()
     {
-        console.log( key + ": " + value.image );
+        //Load the defined image
+        $.each(items, function( key, value ) 
+        {
+            var target = '#img' + key;
 
-        var img_dom = '<img src="' + value.image  + '">';
+            $(target).attr("src", value.image)
+            .error(function() {
+                //this happen if load fails - Load the fail image
+                $(target).attr("src", fail_image);
+            });
+        });
 
-        $('.main').append(img_dom);
-
-    });
-
+    })
 
 
 });
--- resources/views/grid.blade.php
+++ resources/views/grid.blade.php
@@ -3,8 +3,99 @@
 <script src="{{ URL::asset('js/custom/grid.js') }}"></script>
 
 <h1>This is grid test</h1>
+<head>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+        .row::after {
+            content: "";
+            clear: both;
+            display: block;
+        }
+        [class*="col-"] {
+            float: left;
+            padding: 15px;
+        }
+        .col-1 {width: 8.33%;}
+        .col-2 {width: 16.66%;}
+        .col-3 {width: 25%;}
+        .col-4 {width: 33.33%;}
+        .col-5 {width: 41.66%;}
+        .col-6 {width: 50%;}
+        .col-7 {width: 58.33%;}
+        .col-8 {width: 66.66%;}
+        .col-9 {width: 75%;}
+        .col-10 {width: 83.33%;}
+        .col-11 {width: 91.66%;}
+        .col-12 {width: 100%;}
+        html {
+            font-family: "Lucida Sans", sans-serif;
+        }
+        .header {
+            background-color: #9933cc;
+            color: #ffffff;
+            padding: 15px;
+        }
+        .menu ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        .menu li {
+            padding: 8px;
+            margin-bottom: 7px;
+            background-color :#33b5e5;
+            color: #ffffff;
+            box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+        }
+        .menu li:hover {
+            background-color: #0099cc;
+        }
+        img
+        {
+            width: 50px;
+            height: 50px;
+        }
+</style>
+</head>
 <body>
-    <div class="main">
-        
+    <div class="row">
+        <div class="col-1 image load" >
+            Click2Load
+        </div>
+
+        <div class="col-1 image" >
+            <img class="img" id="img0" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img1" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img2" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img3" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img4" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img5" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img6" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img7" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img8" src="">
+        </div>
+        <div class="col-1 image" >
+            <img class="img" id="img9" src="">
+        </div>
+       
+
     </div>
 </body>
