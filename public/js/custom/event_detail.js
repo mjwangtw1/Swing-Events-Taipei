e79a2a14ef61f101fac16daec84b2999e7d6b041
Mou.wang <mou.wang@uitox.com>
2016-06-02 18:01:48 +0800

    now event_detail just point to one image first

--- public/js/custom/event_detail.js
+++ public/js/custom/event_detail.js
@@ -5,25 +5,31 @@ $(function()
 
     var raining_image_path = '/public/assets/stock/event_in_rain.jpg';
 
+    var featured_image_1_path = '/assets/stock/TAIPEI_LINDY_FESTIVAL_2014.png';
+    var featured_image_2_path = '/assets/stock/YM_SWING.png';
+
     var type = $('.the__name').data('type');
 
-    switch(type)
-    {
-        //Special Event
-        case 1:
-            featured_image_path = 'https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/s851x315/13096080_1709318172689797_2323366522317056287_n.jpg?oh=35c7494bf39d9a5af2214561e43d6f2a&oe=57B39033';
-          break;
-        //Blues
-        case 2:
-            featured_image_path = 'https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/13012637_1689938167939726_3648725892285269119_n.jpg?oh=ab67ced89ff600d9449e5ad4474ca41a&oe=57E1D6EA';
-            break;
-        //Swing(Overall)
-        case 0:
-        default:
-            // Remains the same. // use default;
+    // switch(type)
+    // {
+    //     //Special Event
+    //     case 1:
+    //         featured_image_path = 'https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xtp1/v/t1.0-9/s851x315/13096080_1709318172689797_2323366522317056287_n.jpg?oh=35c7494bf39d9a5af2214561e43d6f2a&oe=57B39033';
+    //       break;
+    //     //Blues
+    //     case 2:
+    //         featured_image_path = 'https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xpt1/v/t1.0-9/13012637_1689938167939726_3648725892285269119_n.jpg?oh=ab67ced89ff600d9449e5ad4474ca41a&oe=57E1D6EA';
+    //         break;
+    //     //Swing(Overall)
+    //     case 0:
+    //     default:
+    //         // Remains the same. // use default;
             
-            break;
-    }
+    //         break;
+    // }
+
+    //Currently just point to YM Swing image
+    featured_image_path = featured_image_2_path;
 
     $('.event_cover').css('background-image', 'url(' + featured_image_path + ')');
 
